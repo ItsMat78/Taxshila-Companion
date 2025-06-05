6e7f01981a0e9c99482145044cf23365a4f027b4
Shreyash Rai <shreyashrai078@gmail.com>
2025-06-05 15:58:15 +0000

    Smaller for the small screens!

--- src/app/login/admin/page.tsx
+++ src/app/login/admin/page.tsx
@@ -83,7 +83,7 @@ export default function AdminLoginPage() {
           
           {/* Logo Section (Left Column on MD+) */}
           <div className="flex flex-col items-center justify-center p-4 sm:p-6 md:w-1/3 md:border-r md:border-border/30">
-            <div className="relative w-20 h-auto sm:w-24 md:w-28 mb-4 md:mb-0">
+            <div className="relative w-16 h-auto sm:w-24 md:w-28 mb-4 md:mb-0"> {/* Adjusted w-20 to w-16 for smaller logo on mobile */}
               <Image
                 src={LOGO_URL}
                 alt="Taxshila Companion Logo"
