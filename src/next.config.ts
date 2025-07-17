44f95752f829eedca7510335ca30835ca643ca71
Shreyash Rai <shreyashrai078@gmail.com>
2025-07-17 09:16:22 +0000

    Alright let's start afresh. Somehow the .env files got deleted or someth

---
+++ src/next.config.ts
@@ -0,0 +1,38 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'drive.google.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  // Allow cross-origin requests from the Firebase Studio preview environment
+  // for a smoother development experience.
+  experimental: {
+    allowedDevOrigins: [
+      'https://*.cloudworkstations.dev',
+      'https://*.firebase.studio',
+    ],
+  },
+};
+
+export default nextConfig;
