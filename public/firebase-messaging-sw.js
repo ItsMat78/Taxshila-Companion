00c9a61165874acc8f0387105b8b31a1e04933ee
Shreyash Rai <shreyashrai078@gmail.com>
2025-08-28 00:23:38 +0000

    An error occurred while setting up push notifications. FirebaseError: Me

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,33 +1,38 @@
 
-// Import the Firebase app and messaging services
-import { initializeApp } from 'firebase/app';
-import { getMessaging, onBackgroundMessage } from 'firebase/messaging/sw';
+// Import the Firebase app and messaging scripts.
+// This is the standard way to load the SDK in a service worker.
+importScripts('https://www.gstatic.com/firebasejs/10.12.3/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.12.3/firebase-messaging-compat.js');
 
-// Your web app's Firebase configuration
-// This needs to be present in the service worker file as well.
+// These variables will be replaced by WebpackInjectPlugin.
+// Do not change them.
 const firebaseConfig = {
-  apiKey: self.FIREBASE_API_KEY,
-  authDomain: self.FIREBASE_AUTH_DOMAIN,
-  projectId: self.FIREBASE_PROJECT_ID,
-  storageBucket: self.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: self.FIREBASE_MESSAGING_SENDER_ID,
-  appId: self.FIREBASE_APP_ID,
-  measurementId: self.FIREBASE_MEASUREMENT_ID,
+    apiKey: self.FIREBASE_API_KEY,
+    authDomain: self.FIREBASE_AUTH_DOMAIN,
+    projectId: self.FIREBASE_PROJECT_ID,
+    storageBucket: self.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: self.FIREBASE_MESSAGING_SENDER_ID,
+    appId: self.FIREBASE_APP_ID,
+    measurementId: self.FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize the Firebase app in the service worker
-const app = initializeApp(firebaseConfig);
-const messaging = getMessaging(app);
+// Initialize the Firebase app in the service worker.
+firebase.initializeApp(firebaseConfig);
 
-// Set up the background message handler
-onBackgroundMessage(messaging, (payload) => {
-  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+// Retrieve an instance of Firebase Messaging so that it can handle background messages.
+const messaging = firebase.messaging();
 
-  // Customize the notification here
+messaging.onBackgroundMessage((payload) => {
+  console.log(
+    '[firebase-messaging-sw.js] Received background message ',
+    payload
+  );
+  
+  // Customize notification here
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: payload.notification.icon || '/logo.png',
+    icon: payload.notification.icon || '/logo.png', // Use a default icon if none is provided
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
